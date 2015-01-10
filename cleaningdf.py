d18a72cf6e987ae5b20da41b121c3488c1f80c81
Ogi Moore <ognyan.moore@jacobs.ucsd.edu>
2015-01-09 22:57:58 -0800

    Plots are now functional
    dataframe is now clean to the point that it's functional, but some more
    filtering needs to be done to cleanup team names that have funky
    additions like (sum 2) and such

--- cleaningdf.py
+++ cleaningdf.py
@@ -3,7 +3,6 @@ __author__ = "ogi"
 
 import pandas as pd
 
-
 def findOpposite(df):
     wrongDate = df['Date'] == '0000-00-00'
     for i in range((len(df[wrongDate]))):
@@ -15,7 +14,7 @@ def findOpposite(df):
         matchSeason = relevantRow.Season.values[0]
         
         try:
-            replacementDate = df[df.Team.str.contains(matchTeam) & df.Opponent.str.contains(matchOpponent) & df.Season.str.contains(matchSeason)].Date.values[0]
+            replacementDate = df[df.Team.str.startswith(matchTeam) & df.Opponent.str.startswith(matchOpponent) & df.Season.str.startswith(matchSeason)].Date.values[0]
         except:
             replacementDate = '0000-00-00'
         
@@ -24,15 +23,43 @@ def findOpposite(df):
             df.loc[rowIndex, 'Date'] = replacementDate
     return df
 
-def gameBefore(df):
+def gameBeforeOrAfter(df):
     wrongDate = df['Date'] == '0000-00-00'
-    for i in range((len(df[wrongDate]))):
-        relevantRow = df[wrongDate][i:i+1]
+    #for i in range((len(df[wrongDate]))):
+    for i in df[wrongDate].index:
+        #relevantRow = df[wrongDate][i:i+1]
+        #rowIndex = relevantRow.Date.index[0]
         
+        matchTeam = df.Team[i]
+        matchSeason = df.Season[i]
         
-        if int(df[wrongDate][i,i+1].Week.values[0] > 1):
-            matchTeam = relevantRow.Team.values[0]
-            matchSeason = relevantRow.Season.values[0]
+        weekPlusOne = str(int(df.Week[i]) + 1)
+        weekMinusOne = str(int(df.Week[i]) - 1)
+        
+        weekAhead = df[df.Team.str.startswith(matchTeam) & df.Season.str.startswith(matchSeason) & df.Week.str.startswith(weekPlusOne)]       
+        
+        if int(weekMinusOne) > 0:        
+            weekBehind = df[df.Team.str.startswith(matchTeam) & df.Season.str.startswith(matchSeason) & df.Week.str.startswith(weekMinusOne)] 
+        else:
+            weekBehind = pd.DataFrame()
+        
+        if not weekAhead.empty:
+            #remove 7 from days and apply to date
+            if int(weekAhead.Date.values[0].split('/')[1]) > 7:
+                replacementDate = weekAhead.Date.values[0]                
+                splitDate = replacementDate.split('/')
+                splitDate[1] = str(int(splitDate[1]) - 7)
+                replacementDate = str(splitDate[0] + '/' + splitDate[1] + '/' + splitDate[2])
+                df.iloc[i].Date = replacementDate
+
+        
+        elif not weekBehind.empty:
+            if int(weekBehind.Date.values[0].split('/')[1]) < 23:                
+                replacementDate = weekBehind.Date.values[0]                
+                splitDate = replacementDate.split('/')
+                splitDate[1] = str(int(splitDate[1]) + 7)
+                replacementDate = str(splitDate[0] + '/' + splitDate[1] + '/' + splitDate[2])
+                df.iloc[i].Date = replacementDate
     
     return df
             
@@ -44,76 +71,92 @@ def isDuplicate(df):
         relevantRow = df[wrongDate][i:i+1]
         
         matchTeam = relevantRow.Team.values[0]
-        matchOpponent = relevantRow.Opponent.values[0]
+        matchTime = relevantRow.Time.values[0]
         matchSeason = relevantRow.Season.values[0]
         matchWeek = relevantRow.Week.values[0]
         
-        if len(df[df.Team.str.contains(matchTeam) &
-                df.Opponent.str.contains(matchOpponent) &
-                df.Season.str.contains(matchSeason) &
-                df.Week.str.contains(matchWeek)]) > 1:
+        if len(df[df.Team.str.startswith(matchTeam) &
+                df.Time.str.startswith(matchTime) &
+                df.Season.str.startswith(matchSeason) &
+                df.Week.str.startswith(matchWeek)]) > 1:
             
-            rowIndex.append(relevantRow.Date.index[0])
+            rowIndex.append(int(relevantRow.Date.index[0]))
     return rowIndex
 
 
-def main():
-    df = pd.read_csv('rawPortlandBasketballData.csv',
-                          header=1,
-                          skip_blank_lines=True,
-                          skipinitialspace=True,
-                          index_col=False,
-                          parse_dates=True,
-                          infer_datetime_format=True,
-                          keep_date_col=True,
-                          low_memory=False)
-    
-    df = df.dropna()
-    df = df.drop_duplicates()
-    
-    # No need for the difference column
-    df = df.drop('Diff', 1)
-    
-    # removing entries with meaningless data
-    df = df[~df['Week'].str.contains('week')]
-    
-    # removing pick to play games
-    df = df[~df['Team'].str.contains('to play')]
-    df = df[~df['Opponent'].str.contains('to play')]
-    
-    df = df[~df['Team'].str.contains('to Play')]
-    df = df[~df['Opponent'].str.contains('to Play')]
-    
-    df = df[~df['Team'].str.contains('P2P')]
-    df = df[~df['Opponent'].str.contains('P2P')]
-    
-    # removing games with no recorded score
-    df = df.rename(columns={'Pts 4': 'Pts4', 'Pts Ag': 'PtsAg'})
-    df = df[~(df.Pts4 == 0)]
-    df = df[~(df.PtsAg == 0)]
-    
-    # removing youth games...
-    df = df[~df.Team.str.contains('Youth')]
-    df = df[~df.Opponent.str.contains('Youth')]
-    
-    df = df[~df.Team.str.contains('youth')]
-    df = df[~df.Opponent.str.contains('youth')]
-    
-    # removing no-shows...
-    df = df[~df.Team.str.contains('no show')]
-    df = df[~df.Opponent.str.contains('no show')]
-    
-        
-    wrongDate = df['Date'] == '0000-00-00'
-    print('Bad Date Entries: ' + str(len(df[wrongDate])))
-    # Fixing problematic dates...
 
-    duplicateEntries = isDuplicate(df)
-    df = df.drop(df.index[duplicateEntries])
+df = pd.read_csv('rawPortlandBasketballData.csv',
+                      header=1,
+                      skip_blank_lines=True,
+                      skipinitialspace=True,
+                      index_col=False,
+                      parse_dates=True,
+                      infer_datetime_format=True,
+                      keep_date_col=True,
+                      low_memory=False)
     
-    wrongDate = df['Date'] == '0000-00-00'
-    print('Bad Date Entries: ' + str(len(df[wrongDate])))    
-    
-    #df = findOpposite(df)
-    
-main()
+df = df.dropna()
+df = df.drop_duplicates()
+
+# No need for the difference column
+df = df.drop('Diff', 1)
+
+# removing entries with meaningless data
+df = df[~df['Week'].str.contains('week')]
+
+# removing pick to play games
+df = df[~df['Team'].str.contains('to play')]
+df = df[~df['Opponent'].str.contains('to play')]
+
+df = df[~df['Team'].str.contains('to Play')]
+df = df[~df['Opponent'].str.contains('to Play')]
+
+df = df[~df['Team'].str.contains('P2P')]
+df = df[~df['Opponent'].str.contains('P2P')]
+
+# removing games with no recorded score
+df = df.rename(columns={'Pts 4': 'Pts4', 'Pts Ag': 'PtsAg'})
+df = df[~(df.Pts4 == 0)]
+df = df[~(df.PtsAg == 0)]
+
+# removing youth games...
+df = df[~df.Team.str.contains('Youth')]
+df = df[~df.Opponent.str.contains('Youth')]
+
+df = df[~df.Team.str.contains('youth')]
+df = df[~df.Opponent.str.contains('youth')]
+
+# removing no-shows...
+df = df[~df.Team.str.contains('no show')]
+df = df[~df.Opponent.str.contains('no show')]
+
+# Resetting Index...
+df = df.reset_index(drop=True)    
+wrongDate = df['Date'] == '0000-00-00'
+print('Starting Bad Date Entries: ' + str(len(df[wrongDate])))
+# Fixing problematic dates...
+
+# Is the entry a duplicate?
+df = df.drop(df.index[isDuplicate(df)])
+df = df.reset_index(drop=True)
+wrongDate = df['Date'] == '0000-00-00'
+print('After removing duplicate entries with 1 bad date entry: ' + str(len(df[wrongDate])))    
+
+
+## Is there an opposite entry of the same game with a good date?
+#df = findOpposite(df)
+#df = df.reset_index(drop=True)
+#wrongDate = df['Date'] == '0000-00-00'
+#print('After seeking opposite entry with correct date: ' + str(len(df[wrongDate])))    
+#
+#
+#df = gameBeforeOrAfter(df)
+#df = df.reset_index(drop=True)
+#wrongDate = df['Date'] == '0000-00-00'
+#print('After seeking games week before and after: ' + str(len(df[wrongDate]))) 
+#
+
+
+df['Date'] = pd.to_datetime(df['Date'])
+
+df.to_pickle('cleanerdf.pkl')
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from matplotlib.dates import date2num
 import seaborn as sns
 
-
+plt.close('all')
 
 sns.set_palette("deep", desat=.6)
 sns.set_context(rc={"figure.figsize": (8, 4)})
@@ -16,52 +16,73 @@ sns.set_context(rc={"figure.figsize": (8, 4)})
 df = pd.read_pickle('cleanerdf.pkl')
 
 
-
-
-terminators = df[df.Team == 'Terminators']
-terminators['Date'] = pd.to_datetime(terminators['Date'])
-terminators = terminators.set_index('Date')
-
-daBears = df[df.Team == 'Da Bears']
-daBears['Date'] = pd.to_datetime(daBears['Date'])
-daBears = daBears.set_index('Date')
-
-
+team = 'Rogue'
+teamdf = df[df.Team == team]
+teamdf = teamdf.set_index('Date')
 
 fig1 = plt.figure()
-ax1 = plt.subplot(2,1,1, title="Terminators Points For Histogram")
-terminators.Pts4.plot(kind='hist', alpha=0.5)
-daBears.Pts4.plot(kind='hist', alpha=0.5)
-plt.legend(['Termiators', 'Da Bears'])
+ax1 = plt.subplot(2,1,1, title=team + ' Points For Histogram')
+teamdf.Pts4.plot(kind='hist', alpha=0.5, normed=True)
 
-
-ax2 = plt.subplot(2,1,2, sharex=ax1, title="Terminators Points Against Histogram")
-terminators.PtsAg.plot(kind='hist', alpha=0.5)
-daBears.PtsAg.plot(kind='hist', alpha=0.5)
-plt.legend(['Termiators', 'Da Bears'])
+ax2 = plt.subplot(2,1,2, title=team + ' Points Against Histogram', sharex=ax1)
+teamdf.PtsAg.plot(kind='hist', alpha=0.5, normed=True)
 plt.show()
 
 
 
-maGames = 3
-maTerminatorsPts4 = pd.rolling_mean(terminators.Pts4, maGames)
-maDaBearsPts4 = pd.rolling_mean(daBears.Pts4, maGames)
-
-maTerminatorsPtsAg = pd.rolling_mean(terminators.PtsAg, maGames)
-maDaBearsPtsAg = pd.rolling_mean(daBears.PtsAg, maGames)
 
 
 
-fig2 = plt.figure()
 
-ax1 = plt.subplot(2, 1, 1, title="Points For 3-Game Moving Average")
-plt.plot(maTerminatorsPts4.index, maTerminatorsPts4)
-plt.plot(maDaBearsPts4.index, maDaBearsPts4)
-plt.lenged(['Terminators', 'Da Bears'])
 
-ax2 = plt.subplot(2, 1, 2, title="Points Against 3-Game Moving Average")
-plt.plot(maTerminatorsPtsAg.index, maTerminatorsPtsAg)
-plt.plot(maDaBearsPtsAg.index, maDaBearsPtsAg)
-plt.lenged(['Terminators', 'Da Bears'])
 
-plt.show()
+
+
+#terminators = df[df.Team == 'Terminators']
+#terminators['Date'] = pd.to_datetime(terminators['Date'])
+#terminators = terminators.set_index('Date')
+#
+#daBears = df[df.Team == 'Da Bears']
+#daBears['Date'] = pd.to_datetime(daBears['Date'])
+#daBears = daBears.set_index('Date')
+#
+#
+#
+#fig1 = plt.figure()
+#ax1 = plt.subplot(2,1,1, title="Terminators Points For Histogram")
+#terminators.Pts4.plot(kind='hist', alpha=0.5, normed=True)
+#daBears.Pts4.plot(kind='hist', alpha=0.5, normed=True)
+#plt.legend(['Termiators', 'Da Bears'])
+#
+#
+#ax2 = plt.subplot(2,1,2, sharex=ax1, title="Terminators Points Against Histogram")
+#terminators.PtsAg.plot(kind='hist', alpha=0.5, normed=True)
+#daBears.PtsAg.plot(kind='hist', alpha=0.5, normed=True)
+#plt.legend(['Termiators', 'Da Bears'])
+#plt.show()
+#
+#
+#
+#maGames = 3
+#maTerminatorsPts4 = pd.rolling_mean(terminators.Pts4, maGames)
+#maDaBearsPts4 = pd.rolling_mean(daBears.Pts4, maGames)
+#
+#maTerminatorsPtsAg = pd.rolling_mean(terminators.PtsAg, maGames)
+#maDaBearsPtsAg = pd.rolling_mean(daBears.PtsAg, maGames)
+#
+#
+#
+#fig2 = plt.figure()
+#
+#ax1 = plt.subplot(2, 1, 1, title="Points For 3-Game Moving Average")
+#plt.plot(maTerminatorsPts4.index, maTerminatorsPts4)
+#plt.plot(maDaBearsPts4.index, maDaBearsPts4)
+#plt.legend(['Terminators', 'Da Bears'])
+#plt.ylim(ymin=0)
+#
+#ax2 = plt.subplot(2, 1, 2, title="Points Against 3-Game Moving Average")
+#plt.plot(maTerminatorsPtsAg.index, maTerminatorsPtsAg)
+#plt.plot(maDaBearsPtsAg.index, maDaBearsPtsAg)
+#plt.legend(['Terminators', 'Da Bears'])
+#plt.ylim(ymin=0)
+#plt.show()
